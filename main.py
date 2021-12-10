844166d97f08b741b0accdd200f9cf6622ae32b0
Samieh Abdeljabbar <abdels6375@gmail.com>
2021-12-10 15:17:59 -0600

    update

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 # Making a Discord Bot!!
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
+from os import getenv
+load_dotenv()
 
 bot = commands.Bot(command_prefix='.')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.reply('Hello!')
+
+@bot.command()
+async def add(ctx, num1:float, num2:float):
+    await ctx.reply(num1+num2)
+
+
+bot.run(getenv('TOKEN'))
